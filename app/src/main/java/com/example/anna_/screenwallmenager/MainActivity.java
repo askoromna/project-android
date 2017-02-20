9c3b6b63c52f7d208244d6002effb3cfe65c0672
Hanna Skoromna <hanna.skoromna@gmail.com>
2017-02-20 14:54:26 +0100

    first commit

---
+++ app/src/main/java/com/example/anna_/screenwallmenager/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.anna_.screenwallmenager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import static com.example.anna_.screenwallmenager.R.styleable.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClickPlay(View v) {
+//        ImageButton btn_play = (ImageButton)findViewById(R.id.btn_play);
+//        ImageButton btn_pause = (ImageButton)findViewById(R.id.btn_pause);
+
+        //when play is clicked show pause button and hide play button
+//        int visible = btn_play.getVisibility();
+//        if (visible == 1){
+//            btn_play.setVisibility(v.INVISIBLE);
+//            btn_pause.setVisibility(v.VISIBLE);
+
+        }
+//        if (visible == 0){
+//            btn_play.setVisibility(v.VISIBLE);
+//            btn_pause.setVisibility(v.INVISIBLE);
+//
+//        }
+
+    }
+
